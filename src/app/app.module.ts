d4329544e46c4d2854f7af223e63fd85da58209b
divineLush <homeoflucifer@gmail.com>
2021-06-13 17:15:56 +0700

    implementing directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import BasicHighLightDirective from '../app/basic-highlight/basic-highlight.directive';
+import BetterHighLightDirective from '../app/better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighLightDirective,
+    BetterHighLightDirective,
   ],
   imports: [
     BrowserModule
